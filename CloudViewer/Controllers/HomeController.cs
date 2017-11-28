83352ec0b56016709f7d31c494cc175e920dfa5c
Sasha Kazyrevich <a.kazyrevich@gmail.com>
2017-11-28 10:52:57 +0100

    Add AWS logger

--- CloudViewer/Controllers/HomeController.cs
+++ CloudViewer/Controllers/HomeController.cs
@@ -14,7 +14,7 @@ namespace CloudViewer.Controllers
         private readonly ILogger<HomeController> _logger;
 
         public HomeController(ILogger<HomeController> logger)
-        {
+        {   
             _logger = logger;
         }
 
